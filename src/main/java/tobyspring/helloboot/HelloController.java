116b007e781f17a2976cf09f2e5f53df0a773811
huhdy32 <mong3268@gmail.com>
2023-12-15 18:16:53 +0900

    level: 빈 생명주기

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,8 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +11,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@TempController
+@Controller
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
-/*   디스패처 서블릿은 빈을 다~ 뒤져.
-    이중에 매핑 정보를 가지고 있는 빈 객체를 찾아서 매핑 테이블이란걸 만들어.
-    근데 빈이 나중에는 몇천개가 될 수도 있는데 이는 오버헤드를 유발하겠는데?
-    아. 컨트롤러인 빈은 따로 명시를 해 두자. → @RequestMapping*/
     @GetMapping("/hello")
     @ResponseBody
     public String hello(String name) {
-        // 컨트롤러의 주요 역할 중 하나는 사용자 요청을 검증하는 것임
         return helloService.sayHello(name);
     }
-    /*디스패처 서블릿은 함수의 리턴값이 어떤건지를 기본값으로 확인한다.
-    따라서 우린 클래스의 메소드가 리턴하는 값이
-    뷰의이름인지 혹은 그냥 TextPlain인지를 구분할 수 있도록 해야한다.*/
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,23 +13,43 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        System.out.println("WEBSERVER 등록");
+        return new TomcatServletWebServerFactory();
+    }
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        System.out.println("DISPATCHER 등록");
+        return new DispatcherServlet();
+    }
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//                dispatcherServlet.setApplicationContext(this);
+//                야 이거 컨텍스트도 지정안해줬는데 어떻게 했냐?
+//                범인은 스프링이야. 스프링이 씨발 그냥 집어넣었데.
+//                이건 빈의 생명주기 ( 빈의 라이프 사이클 을 볼 줄 알아야해 )
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
                     servletContext.addServlet("Dispatcher Servlet",
-                                    new DispatcherServlet(this))
+                                    dispatcherServlet)
                             .addMapping("/*");
 
                 });
                 webServer.start();
             }
         };
+        System.out.println(100);
         applicationContext.register(HellobootApplication.class);
+        System.out.println(200);
         applicationContext.refresh();
+        System.out.println(applicationContext.getBean(DispatcherServlet.class));
+        System.out.println(300);
     }
 
 
