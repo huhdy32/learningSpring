bc392dac6581d0dfc178f57f8d5b50d5b4a15135
huhdy32 <mong3268@gmail.com>
2023-12-15 18:43:37 +0900

    level: Spring Application 최종 구현

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -23,35 +24,9 @@ public class HellobootApplication {
         System.out.println("DISPATCHER 등록");
         return new DispatcherServlet();
     }
-    public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this);
-//                야 이거 컨텍스트도 지정안해줬는데 어떻게 했냐?
-//                범인은 스프링이야. 스프링이 씨발 그냥 집어넣었데.
-//                이건 빈의 생명주기 ( 빈의 라이프 사이클 을 볼 줄 알아야해 )
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("Dispatcher Servlet",
-                                    dispatcherServlet)
-                            .addMapping("/*");
-
-                });
-                webServer.start();
-            }
-        };
-        System.out.println(100);
-        applicationContext.register(HellobootApplication.class);
-        System.out.println(200);
-        applicationContext.refresh();
-        System.out.println(applicationContext.getBean(DispatcherServlet.class));
-        System.out.println(300);
+    public static void main(String... args) {
+        MySpringApplication.run(HellobootApplication.class, args);
+//        SpringApplication.run(HellobootApplication.class, args);
     }
-
-
 }
 
