becd557717bea15c37628d477d2d7c4463c6f9de
huhdy32 <mong3268@gmail.com>
2023-12-15 16:19:05 +0900

    level: 팩토리 메소드를 이용한 빈 등록

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -4,11 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
-@Controller
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,35 +1,57 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.cglib.proxy.Dispatcher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.swing.text.AbstractDocument;
-import java.io.IOException;
-
+// 스프링 컨테이너가 이 빈이 빈을 만들어내는 팩토리 메소드를 가지는 객체구나임을 인식 시켜야 함
+@Configuration
 public class HellobootApplication {
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        // 지금 까지 쓰던 요놈은 자바코드로 작성된 Configuration 코드를 읽을 수 없음 따라서 수정
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
                 ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
                 WebServer webServer = serverFactory.getWebServer(servletContext ->
                         servletContext.addServlet("Dispatcher Servlet",
-                                        new DispatcherServlet(this)
-                                ).addMapping("/*")
+                                new DispatcherServlet(this))
+                                .addMapping("/*")
                 );
                 webServer.start();
             }
         };
+        applicationContext.register(HellobootApplication.class);
+        applicationContext.refresh();
+         /*좋아, 하지만 이번엔 복잡한 객체를 위한 팩토리 메소드를 만들자. 팩토리 메소드를 통해 구현 해보자!!
+
+         어차피 우리가 쓸 팩토리 메소드는 Spring Container에서 관리해. 지가 팩토리 메소드임을 확인하고 실행시킬것임
+         AnnotationConfigApplicationContext 컨테이너는 요놈 지원 안해. 팩토리메소드로만 받는다.
+         하지만 빈을 구성할 팩토리 메소드를 가진 클래스를 등록 해줘야 한다.
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
-        //이제 서블릿 컨테이너의 서블릿에 매핑정보를 넣는게 아니라, 스프링 컨테이너에 존재하는 컨트롤러에 매핑정보를 등록해버리자!!
-        // 이를테면 GetMapping, PostMapping 등의 메소드가 존재함 또한
+        applicationContext.refresh();*/
+    }
+
+    //
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    // 여기서 우리 리턴타입을 빈을 주입받을 객체에서 기대하는 값으로 정해주도록 하자.!!!
+    // <인터페이스 타입으로 리턴하도록 하세요>
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
     }
 }
 
